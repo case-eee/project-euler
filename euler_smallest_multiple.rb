3099b2490cac96c5840fdb1a89557e1f44836dbc
Casey Ann Cumbow <cumbow8@gmail.com>
2015-06-14 12:26:45 -0500

    small refactoring of smallest multiple

--- euler_smallest_multiple.rb
+++ euler_smallest_multiple.rb
@@ -1,65 +1,57 @@
 require 'prime'
 
 def all_primes(n)
-  @all = []
-  (1..Math.sqrt(n).floor).each do |num| 
-    if num.prime?
-      @all << num
-    end
+  (1..Math.sqrt(n).floor).each_with_object(Array.new) do |num, primes|
+    primes << num if num.prime?
   end
-  @all
 end
 
 def prime_factorization(n)
-  all_primes(n)
+  primes = all_primes(n)
   counter = 0
   factors = []
-  until counter >= @all.length
-    if n % @all[counter] != 0
+  until counter >= primes.length
+    if n % primes[counter] != 0
       counter += 1
     else
-      factors << @all[counter]
-      n /= @all[counter]
+      factors << primes[counter]
+      n /= primes[counter]
     end
   end
-  factors << n unless n == 1 
+  factors << n unless n == 1
   factors
 end
 
 def each_prime_factorization(n)
-  @all_arrays = []
-  (2..n).each do |number|
-    @all_arrays << prime_factorization(number)
+  (2..n).each_with_object(Array.new) do |number, factors|
+    factors << prime_factorization(number)
   end
-  @all_arrays
 end
 
 def final_factors(n)
-  each_prime_factorization(n)
+  all_prime_factors = each_prime_factorization(n)
   number = 2
-  @final_factors_array = []
+  final_factors_array = []
 
-  until number > @all_arrays.length
+  until number > all_prime_factors.length
     count_of_number = []
-    @all_arrays.each do |array|
+    all_prime_factors.each do |array|
       count_of_number << array.count(number)
     end
     total_of_number = count_of_number.sort.last
-    total_of_number.times do 
-      @final_factors_array << number
+    total_of_number.times do
+      final_factors_array << number
     end
     number += 1
   end
 
-  @final_factors_array
-  p @final_factors_array
+  final_factors_array
 end
 
 def smallest_multiple(n)
-  final_factors(n)
-  @final_factors_array.inject(:*)
+  final_factors(n).inject(:*)
 end
 
 
-p smallest_multiple(10)
+p smallest_multiple(20) == 232792560
 
